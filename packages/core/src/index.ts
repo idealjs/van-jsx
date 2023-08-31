de2b526eae50b717f35371c950ea7277d1fad57a
cqh963852 <cqh963852@hotmail.com>
2023-08-31 09:36:54 +0800

    fix: update createState return type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,16 +1,12 @@
 import van, { State, StateView } from "vanjs-core";
 
-export interface ReadonlyState<T> extends StateView<T> {
-  readonly val: T;
-}
-
 export const lazy = <T>(f: () => T) => {
   return f as unknown as T;
 };
 
 export function createState<T>(initialValue: T): State<T>;
 
-export function createState<T>(initialValue: T | null): ReadonlyState<T>;
+export function createState<T>(initialValue: T | null): StateView<T>;
 
 export function createState<T = undefined>(): State<T | undefined>;
 
